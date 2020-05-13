a2502a9e957c3f54f555266c8a6cbbfc7296511c
Niels van der meer <you@example.com>
2020-05-13 12:51:54 +0200

    opdracht 8

---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $voornaam1 = niels;
+        $voornaam2 = niels;
+        $voornaam3 = niels;
+        echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+    ?>
+</body>
+</html>
