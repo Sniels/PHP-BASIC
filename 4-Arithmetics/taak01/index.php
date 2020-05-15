e2236fe695b4f6405444e6c80a5cf14408bbecff
Niels van der meer <you@example.com>
2020-05-15 10:03:57 +0200

    opdrachten af

---
+++ 4-Arithmetics/taak01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $getal1 = 10;
+        $getal2 = 15;
+        echo $getal1 + $getal2;
+        echo $getal1 - $getal2;
+        echo $getal1 * $getal2;
+        echo $getal1 / $getal2;
+    ?>
+</body>
+</html>
---
+++ 4-Arithmetics/taak02/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $getal1 = 10;
+        $getal2 = 10;
+        $getal3 = 10;
+        $getal4 = 10;
+        $getal1 += 10;
+        $getal2 -= 10;
+        $getal3 *= 10;
+        $getal4 /= 10;
+        echo $getal1;
+        echo "<br>";
+        echo $getal2;
+        echo "<br>";
+        echo $getal3;
+        echo "<br>";
+        echo $getal4;
+    ?>
+</body>
+</html>
