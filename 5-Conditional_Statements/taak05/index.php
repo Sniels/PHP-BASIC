9b6ca9a782c078e696894f0fc9014b56c5e48e3f
Niels van der meer <you@example.com>
2020-05-19 12:22:30 +0200

    taak 5

--- 5-Conditional_Statements/taak05/index.php
+++ 5-Conditional_Statements/taak05/index.php
@@ -6,6 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        $score = 15;
+
+        switch($score) {
+            case 10:
+            echo "Wow wat een fantastische score!";
+            break;
+            case 9:
+            echo "Wow bijna alle punten nog 1 te gaan!";
+            break;
+            case 8:
+            echo "Goed bezig, bijna alle punten!";
+            break;
+            case 7:
+            echo "Ga zo door!";
+            break;
+            default:
+            echo "Nog even oefenen dan haal je vast alle punten!";
+        }
+    ?>
 </body>
 </html>
